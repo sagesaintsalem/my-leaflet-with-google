beb8a0a03a957e055c8b64062cddc86f2f710450
Rita Toussaint <85616342+sagesaintsalem@users.noreply.github.com>
2023-12-13 11:29:25 +0000

    Update google-doc-url.js

--- google-doc-url.js
+++ google-doc-url.js
@@ -2,4 +2,4 @@
 var googleDocURL = 'https://docs.google.com/spreadsheets/d/13Y5g1dyLmcCFBLtk7fPAK9PFpfgzk25GXY9MA2hk0co/edit#gid=0';
 
 // insert your own Google Sheets API key from https://console.developers.google.com
-var googleApiKey = 'AIzaSyBh9nKnVZm2RPeZa0ywCOxPAgJJfK87WhY';
+var googleApiKey = 'AIzaSyAXPa5wwUT6VMEfZdwxxtCD37CUtb0wFhE';
